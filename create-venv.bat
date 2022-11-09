ec4772f256fd7a582ad207e524ec65efd12b5023
Adam Alder <atariq11700@gmail.com>
2022-11-08 18:17:20 -0700

    python version 3.8.10

--- create-venv.bat
+++ create-venv.bat
@@ -1,4 +1,4 @@
 pip install virtualenv
-virtualenv venv-5510-spot --python=python3.8.15
+virtualenv venv-5510-spot --python=python3.8.10
 venv-5510-spot/scripts/activate.bat
 pip install -r requirements.txt
--- create-venv.sh
+++ create-venv.sh
@@ -1,4 +1,4 @@
 pip install virtualenv
-virtualenv venv-5510-spot --python=python3.8.15
+virtualenv venv-5510-spot --python=python3.8.10
 source venv-5510-spot/scripts/activate
 pip install -r requirements.txt
