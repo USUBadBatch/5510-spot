92966de926defd61acae3d0a8acb5cbabae0afcf
Adam Alder <atariq11700@gmail.com>
2022-11-08 19:29:09 -0700

    updated setup scripts

--- create-venv.bat
+++ create-venv.bat
@@ -2,4 +2,3 @@ pip install virtualenv
 virtualenv venv-5510-spot --python=python3.8.10
 venv-5510-spot/scripts/activate.bat
 pip install -r requirements.txt
-venv-5510-spot/scripts/activate.bat
--- create-venv.sh
+++ create-venv.sh
@@ -2,4 +2,3 @@ pip install virtualenv
 virtualenv venv-5510-spot --python=python3.8.10
 source venv-5510-spot/scripts/activate
 pip install -r requirements.txt
-source venv-5510-spot/scripts/activate
