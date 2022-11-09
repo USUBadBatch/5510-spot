dc159edf6e26d52cb88d1efd4da202e4f6ad6084
Adam Alder <atariq11700@gmail.com>
2022-11-08 18:15:20 -0700

    added setup files

---
+++ create-venv.bat
@@ -0,0 +1,4 @@
+pip install virtualenv
+virtualenv venv-5510-spot --python=python3.8
+venv-5510-spot/scripts/activate.bat
+pip install -r requirements.txt
---
+++ create-venv.sh
@@ -0,0 +1,4 @@
+pip install virtualenv
+virtualenv venv-5510-spot --python=python3.8
+source venv-5510-spot/scripts/activate
+pip install -r requirements.txt
